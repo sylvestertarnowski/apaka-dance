eddb88d1b79ee95df0c4555753cced88213586d0
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-05-30 07:26:20 +0200

    add header positioning, add header buttons alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,16 @@ const Header: React.FC = () => {
     return (
         <div className="header">
             <WidthWrapper>
-                Header Upper
-                Header Lower
+                <div className="header-container">
+                    <div id="header-logo-button">alpakaDance</div>
+                    <div id="header-nav-buttons">
+                        <div>Strona Główna</div>
+                        <div>Zajęcia</div>
+                        <div>Usługi</div>
+                        <div>O mnie</div>
+                        <div>Kontakt</div>
+                    </div>
+                </div>
             </WidthWrapper>
         </div>
     )
--- src/layout/WidthWrapper.tsx
+++ src/layout/WidthWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WidthWrapper: React.FC = ({ children }) => {
     return (
-        <div className="width-wrapper" style={{ maxWidth: "1000px" }}>
+        <div className="width-wrapper" style={{ maxWidth: "1000px", flex: 1 }}>
             {children}
         </div>
     )
