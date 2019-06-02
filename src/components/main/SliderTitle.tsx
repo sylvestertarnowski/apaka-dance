67a25bab27036a4961dfb1ea1fe99568bbe2b6a5
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-06-02 08:24:28 +0200

    add SliderTitle style sheet, add SliderTitle elements

--- src/components/main/SliderTitle.tsx
+++ src/components/main/SliderTitle.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import '../../css/main/SliderTitle.css';
 
 class SliderTitle extends React.Component {
     render() {
         return (
-            <div></div>
+            <div className="slider-title-container">
+                <div>{"<-"}</div>
+                <div>Grow your dancing Abilities!</div>
+                <div>{"->"}</div>
+            </div>
         )
     }
 }
