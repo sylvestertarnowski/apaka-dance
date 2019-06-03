3804bd83cf949a57e07cfed08d324157b055af0c
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-06-03 08:25:00 +0200

    add slidertitle alternative text

--- src/components/main/SliderTitle.tsx
+++ src/components/main/SliderTitle.tsx
@@ -3,10 +3,12 @@ import '../../css/main/SliderTitle.css';
 
 class SliderTitle extends React.Component {
     render() {
+        const divOne = <div>Czerp radość z tańca</div>;
+        const divTwo = <div>Bądź najlepszym tancerze na dzielni</div>;
         return (
             <div className="slider-title-container">
                 <div>{"<-"}</div>
-                <div>Grow your dancing Abilities!</div>
+                {divOne}
                 <div>{"->"}</div>
             </div>
         )
