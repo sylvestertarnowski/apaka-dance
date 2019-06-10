3c596ada6b6b8e69809d09df19b639d9f1cc511d
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-06-10 20:00:46 +0200

    change DanceTypes image description to be transparent, and only reveal when hovered over. change DanceTypes to be Flex container instead of grid

--- src/components/main/ImageInfo.tsx
+++ src/components/main/ImageInfo.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
+import '../../css/main/ImageInfo.css';
 
 type P = {
-    imgAlt: string;
     imgSrc: string;
     danceType: string;
     description: string;
 }
 
 const ImageInfo: React.FC<P> = (props) => {
-    const { imgAlt, imgSrc, danceType, description } = props;
+    const { imgSrc, danceType, description } = props;
+    const styles = {
+        backgroundImage: `url(${imgSrc})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    }
 
     return (
-        <div>
-            <img src={imgSrc} alt={imgAlt} />
-            <div>
+        <div className="image-info-container" style={styles}>
+            <div className="image-info-description">
                 <h2>{danceType}</h2>
                 <p>{description}</p>
             </div>
