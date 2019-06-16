9c0ebfddc44c44f4f661edcf0460fcb2ab49d304
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-06-16 16:05:48 +0200

    add stylesheet to RoundSidesButton

--- src/components/layout/RoundSidesButton.tsx
+++ src/components/layout/RoundSidesButton.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
+import '../../css/layout/RoundSidesButton.css';
 
 interface P {
     description: string;
-    color?: string;
+    backgroundColor?: string;
+    textColor?: string;
 }
 
 const RoundSidesButton: React.FC<P> = (props) => {
-    const { description } = props;
+    const { description, backgroundColor, textColor } = props;
+    const styles = {
+        backgroundColor: backgroundColor ? backgroundColor : "rgb(242, 58, 45)",
+        color: textColor ? textColor : "#fff",
+    } as React.CSSProperties;
 
     return (
-        <button className="round-side-button">
+        <button className="round-sides-button" style={styles}>
             {description}
         </button>
     )
